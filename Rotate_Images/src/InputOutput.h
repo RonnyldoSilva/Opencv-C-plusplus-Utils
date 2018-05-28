7554cacf6524e21cc82434fa3f3652912ee7f0cc
ronnyldo <ronnyldo.silva@ccc.ufcg.edu.br>
2018-05-28 09:09:34 -0300

    Refactoring InputOutput.h

--- Rotate_Images/src/InputOutput.h
+++ Rotate_Images/src/InputOutput.h
@@ -1,10 +1,3 @@
-/*
- * InputOutput.h
- *
- *  Created on: Jul 4, 2017
- *      Author: carlos
- */
-
 #ifndef INPUTOUTPUT_H_
 #define INPUTOUTPUT_H_
 
@@ -13,47 +6,47 @@
 #include <vector>
 
 /**
- * Classe usada para fazer entrada e saida de arquivos com o HD
+ * Class used to input and output files with Hard Driver
  */
+
 class InputOutput {
-public:
-	InputOutput();
 
-	/**
-	 * Método que carrega o arquivo de configuração (JSON) do classificador do disco
-	 * @param filename, nome do arquivo JSON salvo no HD
-	 * @return mapa chave valor, que corresponde as chaves e os campos do arquivo JSON passado por parâmetro
-	 */
-	static std::map<std::string, std::string> readJson(std::string filename);
+	public:
+		InputOutput();
 
-	/**
-	 * Método que carrega do disco o arquivo que contém os nomes das classe e seu indice correspondente
-	 * @param fileName, come do arquivo txt salvo no HD
-	 * @return mapa chave valor, que corresponde ao indice e o nome das classes
-	 */
-	static std::map<int, std::string> readIndexLabelFile(std::string fileName);
+		/**
+		 * Method that loads the disk classifier configuration file (JSON)
+		 * @param filename, JSON file name saved in HD
+		 * @return map key value, which matches the keys and fields of the JSON file passed by parameter
+		 */
+		static std::map<std::string, std::string> readJson(std::string filename);
 
-	/**
-	 * Método que retorna um mapa contendo o nome das classes como chave e o indice dela como valor
-	 * @param labelIndex, mapa contendo o indice da classe como chave e o nome dela como valor
-	 * @return mapa contendo o nome das classes como chave e o indice dela como valor
-	 */
-	static std::map<std::string, int> convertIndexLabelFile(std::map<int, std::string> labelIndex);
+		/**
+		 * Method that loads the file containing the class names from the disk and its corresponding index
+		 * @param fileName, come from txt file saved in HD
+		 * @return map key value, which corresponds to the index and the name of the classes
+		 */
+		static std::map<int, std::string> readIndexLabelFile(std::string fileName);
 
-	/**
-	 * Método usado para realizar um split numa string
-	 * @param str, string de entrada que será usada para computar os tokens
-	 * @param tokens, resultado do split
-	 * @param delimiters, delimitador que será usado no split
-	 */
-	static void tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters);
+		/**
+		 * Method that returns a map containing the class names as key and its index as value
+		 * @param labelIndex, map containing the index of the class as key and its name as value
+		 * @return map containing the name of the classes as key and the index of it as value
+		 */
+		static std::map<std::string, int> convertIndexLabelFile(std::map<int, std::string> labelIndex);
 
-	static std::vector<std::string> read_ocr_result(std::string filename);
-	static std::map<char, char> read_dict(std::string filename);
-	static std::vector<std::string> getName(std::string pathDir);
+		/**
+		 * Method used to perform a split in a string
+		 * @param str, input string that will be used to compute the tokens
+		 * @param tokens, split result
+		 * @param delimiters, delimiter that will be used in split
+		 */
+		static void tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters);
+		static std::vector<std::string> read_ocr_result(std::string filename);
+		static std::map<char, char> read_dict(std::string filename);
+		static std::vector<std::string> getName(std::string pathDir);
 
-
-	virtual ~InputOutput();
+		virtual ~InputOutput();
 };
 
 #endif /* INPUTOUTPUT_H_ */
